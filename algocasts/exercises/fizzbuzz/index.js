03f33708a4faa264be2092eee7a14ff827ce8763
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-14 14:39:28 -0700

    Solved fizzbuzz challenge

--- algocasts/exercises/fizzbuzz/index.js
+++ algocasts/exercises/fizzbuzz/index.js
@@ -11,7 +11,39 @@
 //   fizz
 //   4
 //   buzz
+function divisibleByThree(n){
+    return n%3 === 0;
+}
 
-function fizzBuzz(n) {}
+function divisibleByFive(n){
+    return n%5 === 0;
+}
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++){
+        let output;
+
+        // divisible by 3 & 5
+        if (divisibleByFive(i) && divisibleByThree(i)){
+            output = 'fizzbuzz';
+        }
+
+        // divisible by 3
+        else if (divisibleByThree(i)){
+            output = 'fizz'
+        }
+
+        // divisible by 5
+        else if (divisibleByFive(i)){
+            output = 'buzz';
+        }
+
+        else {
+            output = i;
+        }
+
+        console.log(output);
+    }
+}
 
 module.exports = fizzBuzz;
