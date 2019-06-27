dde12e0a51e46c85863348d7302439434149012c
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-27 12:19:00 -0700

    built a queue using a linkedlist

--- algocasts/exercises/linkedlist/index.js
+++ algocasts/exercises/linkedlist/index.js
@@ -6,4 +6,34 @@ class Node {}
 
 class LinkedList {}
 
-module.exports = { Node, LinkedList };
+function reverseLinkedList(node){
+    let prev = null;
+    let nextNode = null;
+    let currentNode = node;
+
+    while (currentNode.next !== null){
+        nextNode = currentNode.next;
+        currentNode.next = prev;
+
+        prev = currentNode;
+        currentNode = nextNode;
+    }
+
+    return currentNode;
+}
+
+function remove(val){
+    let temp  = this.head;
+
+    while(temp.next !== null){
+        if (temp.val === val){
+            temp.val = temp.next.val;
+            temp.next = temp.next.next;
+            return temp.val;
+        }
+
+    }
+    return -1;
+}
+
+module.exports = { Node, LinkedList, reverseLinkedList };
--- algocasts/exercises/queue/index.js
+++ algocasts/exercises/queue/index.js
@@ -7,7 +7,38 @@
 //     const q = new Queue();
 //     q.add(1);
 //     q.remove(); // returns 1;
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class Queue {
+    constructor(){
+        this.head = null;
+        this.tail = this.head;
+    }
 
-class Queue {}
+    add (val) {
+        if (!this.head){
+            this.head = new Node(val);
+            this.tail = this.head;
+        } else {
+            let tempNode = new Node(val);
+            this.tail.next = tempNode;
+            this.tail = tempNode;
+        }
+    }
+
+    remove (){
+        if (!this.head){
+            return undefined;
+        } else {
+            let temp = this.head.val;
+            this.head = this.head.next;
+            return temp;
+        }
+    }
+}
 
 module.exports = Queue;
