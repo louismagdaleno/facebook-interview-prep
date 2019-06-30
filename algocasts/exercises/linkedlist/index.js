9f26cb430f29a3458cfb8e78053a11a2666a30ad
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-29 19:46:44 -0700

    Implemented BFS and DFS for tree

--- algocasts/exercises/linkedlist/index.js
+++ algocasts/exercises/linkedlist/index.js
@@ -2,38 +2,74 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(value, next){
+        this.data = value,
+        this.next = next || null,
+        this.previous = null
+    };
+};
 
-class LinkedList {}
-
-function reverseLinkedList(node){
-    let prev = null;
-    let nextNode = null;
-    let currentNode = node;
-
-    while (currentNode.next !== null){
-        nextNode = currentNode.next;
-        currentNode.next = prev;
-
-        prev = currentNode;
-        currentNode = nextNode;
+class LinkedList {
+    constructor(){
+        this.head = null,
+        this.tail = this.head,
+        this.size = 0
     }
 
-    return currentNode;
-}
+    insertFirst(data){
+        const newNode = new Node(data);
 
-function remove(val){
-    let temp  = this.head;
-
-    while(temp.next !== null){
-        if (temp.val === val){
-            temp.val = temp.next.val;
-            temp.next = temp.next.next;
-            return temp.val;
+        if (this.length > 0){
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            this.head = newNode;
+            this.tail = newNode;
         }
-
+        
+        let temp = head;
+        while (temp.next !== null){
+            size++;
+            temp = temp.next;
+        }
     }
-    return -1;
+
+    getFirst(){
+        return this.head;
+    }
+
+    getLast(){
+        return this.tail;
+    }
+    clear(){
+        this.head = null;
+        this.tail = this.head;
+        this.size = 0;
+    }
+    removeFirst(){
+        if(this.head.next){
+            this.head = this.head.next;
+            this.size--;
+        } else {
+            this.head = null;
+            this.tail = this.head;
+            this.size = 0;
+        }
+       
+    }
+    removeLast(){
+        let temp = this.head;
+
+        while (temp.next.next !== null ){
+            if (temp.next.next === null){
+                this.tail
+            }
+        }
+    }
+
 }
 
-module.exports = { Node, LinkedList, reverseLinkedList };
+
+
+module.exports = { Node, LinkedList };
